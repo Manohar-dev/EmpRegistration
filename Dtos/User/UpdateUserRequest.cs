2b5501f43588c4002d2914ae8bd461d81bd3ae07
Manohar-dev <119424432+Manohar-dev@users.noreply.github.com>
2024-05-16 14:08:21 +0530

    Delete Dtos directory

--- Dtos/User/UpdateUserRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace backend.Dtos.User
-{
-    public class UpdateUserRequest()
-    {
-        public string? username { get; set; }
-         public IList<string?>? comments { get; set; }
-    }
-}
